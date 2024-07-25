44516fd872cb1fd34c95d1492dca343b7caa9cdf
minisun <minisundev@gmail.com>
2024-07-25 16:15:19 +0900

    feat: Add method for adding employees

---
+++ src/main/java/com/lec/payroll/domain/employee/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.lec.payroll.domain.employee.controller;
+
+import com.lec.payroll.domain.employee.dto.request.EmployeeCreateRequest;
+import com.lec.payroll.domain.employee.service.EmployeeService;
+import com.lec.payroll.domain.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse<Void>> addEmployee(@Valid @RequestBody EmployeeCreateRequest request) {
+        return new ResponseEntity(new ApiResponse<Void>(null, employeeService.addEmployee(request), null), HttpStatus.CREATED);
+    }
+}
