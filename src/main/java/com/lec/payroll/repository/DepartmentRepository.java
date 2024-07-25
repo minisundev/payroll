f8e070e04bf42a1f617f1152a60c84f21969ceda
minisun <minisundev@gmail.com>
2024-07-25 14:19:44 +0900

    feat: Add DepartmentRepository interface

--- src/main/java/com/lec/payroll/repository/DepartmentRepository.java
+++ src/main/java/com/lec/payroll/repository/DepartmentRepository.java
@@ -1,4 +1,9 @@
 package com.lec.payroll.repository;
 
-public class DepartmentRepository {
+import com.lec.payroll.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
