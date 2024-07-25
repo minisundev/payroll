58fe2ccf88f4b63caa75e293a9cf9fa069a86338
minisun <minisundev@gmail.com>
2024-07-25 16:02:01 +0900

    feat: Add Department Enum

---
+++ src/main/java/com/lec/payroll/domain/employee/model/Department.java
@@ -0,0 +1,13 @@
+package com.lec.payroll.domain.employee.model;
+
+public enum Department {
+    FRONT_END,
+    BACK_END,
+    INFRASTRUCTURE,
+    DATA_ANALYTICS,
+    AI,
+    DESIGN,
+    QA,
+    OPERATION,
+    HUMAN_RESOURCE;
+}
