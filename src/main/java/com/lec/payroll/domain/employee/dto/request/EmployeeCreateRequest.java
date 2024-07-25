c8ae231b1ca1ad000b7fb6ef9cd26d8bdec59f0d
minisun <minisundev@gmail.com>
2024-07-25 16:03:24 +0900

    refac: Add Department Enum To EmployeeCreateRequest

--- src/main/java/com/lec/payroll/domain/employee/dto/request/EmployeeCreateRequest.java
+++ src/main/java/com/lec/payroll/domain/employee/dto/request/EmployeeCreateRequest.java
@@ -1,5 +1,6 @@
 package com.lec.payroll.domain.employee.dto.request;
 
+import com.lec.payroll.domain.employee.model.Department;
 import com.lec.payroll.domain.employee.model.Position;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +9,6 @@ public record EmployeeCreateRequest(
         @NotBlank String name,
         @NotNull Integer salary,
         @NotNull Position position,
-        @NotNull Long departmentId
+        @NotNull Department department
 ) {
 }
