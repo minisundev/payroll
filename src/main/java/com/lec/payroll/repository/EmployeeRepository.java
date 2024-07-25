1d0077449a2d7bb3bc7fa676acb0c59be1b8e97d
minisun <minisundev@gmail.com>
2024-07-25 14:20:00 +0900

    feat: Add EmployeeRepository interface

--- src/main/java/com/lec/payroll/repository/EmployeeRepository.java
+++ src/main/java/com/lec/payroll/repository/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.lec.payroll.repository;
 
-public class EmployeeRepository {
+import com.lec.payroll.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
