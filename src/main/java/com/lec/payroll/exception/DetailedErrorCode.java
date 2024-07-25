c43c9fa6995b04a893fcb26d46e7dc546a83759c
minisun <minisundev@gmail.com>
2024-07-25 14:58:30 +0900

    feat: Add DetailedErrorCode

---
+++ src/main/java/com/lec/payroll/exception/DetailedErrorCode.java
@@ -0,0 +1,19 @@
+package com.lec.payroll.exception;
+
+public class DetailedErrorCode implements ErrorCode {
+
+    @Override
+    public String getCode() {
+        return "";
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+
+    @Override
+    public String getHttpStatus() {
+        return "";
+    }
+}
