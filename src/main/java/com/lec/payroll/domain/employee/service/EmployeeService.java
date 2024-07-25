9cd5b45b5db36a79d93cb08c0ca589c8114c7ef7
minisun <minisundev@gmail.com>
2024-07-26 08:55:29 +0900

    feat: Add getEmployees method to EmployeeService

--- src/main/java/com/lec/payroll/domain/employee/service/EmployeeService.java
+++ src/main/java/com/lec/payroll/domain/employee/service/EmployeeService.java
@@ -1,11 +1,13 @@
 package com.lec.payroll.domain.employee.service;
 
 import com.lec.payroll.domain.employee.dto.request.EmployeeCreateRequest;
+import com.lec.payroll.domain.employee.dto.response.EmployeeResponse;
 import com.lec.payroll.domain.employee.model.Employee;
 import com.lec.payroll.domain.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +18,25 @@ public class EmployeeService {
     public void addEmployee(EmployeeCreateRequest request) {
         Employee employee = modelMapper.map(request, Employee.class);
         employeeRepository.save(employee);
+    }
+
+    public List<EmployeeResponse> getEmployees(){
+        List<Employee> employees = employeeRepository.findAll();
+        List<EmployeeResponse> responses = employees
+                .stream()
+                .map(e -> convertEmployeeToResponse(e))
+                .toList();
+        return responses;
+    }
+
+    private EmployeeResponse convertEmployeeToResponse(Employee employee) {
+        EmployeeResponse employeeResponse = EmployeeResponse.builder()
+                .id(employee.getId())
+                .name(employee.getName())
+                .salary(employee.getSalary())
+                .department(employee.getDepartment())
+                .position(employee.getPosition())
+                .build();
+        return employeeResponse;
     }
 }
