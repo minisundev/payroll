575284585df413ae748a22535758fa491fe0d7cc
minisun <minisundev@gmail.com>
2024-07-25 14:20:36 +0900

    feat: Add EmployeeController

--- src/main/java/com/lec/payroll/controller/EmployeeController.java
+++ src/main/java/com/lec/payroll/controller/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.lec.payroll.controller;
 
+import com.lec.payroll.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class EmployeeController {
+    private final EmployeeService employeeService;
 }
