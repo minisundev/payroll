4ea8d2ae0ab888f70985fab7cbf7a10e6798d330
minisun <minisundev@gmail.com>
2024-07-25 17:46:26 +0900

    feat: Add addPayRoll in PayRollController

--- src/main/java/com/lec/payroll/domain/payroll/controller/PayRollController.java
+++ src/main/java/com/lec/payroll/domain/payroll/controller/PayRollController.java
@@ -1,13 +1,30 @@
 package com.lec.payroll.domain.payroll.controller;
 
+import com.lec.payroll.domain.global.response.ApiResponse;
+import com.lec.payroll.domain.payroll.dto.request.PayrollCreateRequest;
+import com.lec.payroll.domain.payroll.dto.response.PayrollResponse;
+import com.lec.payroll.domain.payroll.model.Payroll;
 import com.lec.payroll.domain.payroll.service.PayrollService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/payroll")
 public class PayRollController {
     private final PayrollService payrollService;
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse<Void>> addPayroll(
+            @Valid @RequestBody PayrollCreateRequest request
+    ) {
+        payrollService.addPayroll(request);
+        String msg = "Payroll added successfully";
+        ApiResponse<Void> response = new ApiResponse<Void>(null, msg, null);
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+
 }
