e67aabf9bf4a904c02500fcd206bb84f82d93978
minisun <minisundev@gmail.com>
2024-07-25 17:43:19 +0900

    refac: Change field types in DetailedErrorCode

--- src/main/java/com/lec/payroll/domain/global/exception/DetailedErrorCode.java
+++ src/main/java/com/lec/payroll/domain/global/exception/DetailedErrorCode.java
@@ -1,19 +1,27 @@
 package com.lec.payroll.domain.global.exception;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
 public class DetailedErrorCode implements ErrorCode {
 
     @Override
-    public String getCode() {
-        return "";
+    public Integer getCode() {
+        return code;
     }
 
     @Override
     public String getMessage() {
-        return "";
+        return message;
     }
 
     @Override
-    public String getHttpStatus() {
-        return "";
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
+
+    private final Integer code;
+    private final String message;
+    private final HttpStatus httpStatus;
 }
