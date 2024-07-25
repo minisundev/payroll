cb3be15e3e46cd89176dbd979bfbd7c6d8cd7c54
minisun <minisundev@gmail.com>
2024-07-25 13:42:21 +0900

    fix: fixed imports

--- src/test/java/com/lec/payroll/TestPayrollApplication.java
+++ src/test/java/com/lec/payroll/TestPayrollApplication.java
@@ -1,6 +1,7 @@
 package com.lec.payroll;
 
 import org.springframework.boot.SpringApplication;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 public class TestPayrollApplication {
 
