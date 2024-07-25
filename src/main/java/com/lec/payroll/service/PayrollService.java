ee438faddc55fd548fd29313f6d2a26593005809
minisun <minisundev@gmail.com>
2024-07-25 14:20:19 +0900

    feat: Add PayrollService

--- src/main/java/com/lec/payroll/service/PayrollService.java
+++ src/main/java/com/lec/payroll/service/PayrollService.java
@@ -1,4 +1,11 @@
 package com.lec.payroll.service;
 
+import com.lec.payroll.repository.PayrollRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class PayrollService {
+    private final PayrollRepository payrollRepository;
 }
