e63c63dabd37da33b86af6a279ec8082942eb746
minisun <minisundev@gmail.com>
2024-07-25 14:18:52 +0900

    feat: Add Employee Entity

--- src/main/java/com/lec/payroll/model/Employee.java
+++ src/main/java/com/lec/payroll/model/Employee.java
@@ -1,4 +1,25 @@
 package com.lec.payroll.model;
 
-public class Employee {
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "employees")
+public class Employee extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Integer salary;
+
+    @Column
+    private Position position;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Department department;
 }
