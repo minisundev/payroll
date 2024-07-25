a8c73a0da556cb027d3ca27caf087ba0e609f9a0
minisun <minisundev@gmail.com>
2024-07-25 14:58:55 +0900

    feat: Add ApiResponse

---
+++ src/main/java/com/lec/payroll/dto/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.lec.payroll.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+        int code,
+        String message,
+        T data
+) {
+}
