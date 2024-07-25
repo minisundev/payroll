d6db6f162d7a95e6554be165eb7181cfe43b983a
minisun <minisundev@gmail.com>
2024-07-25 14:18:35 +0900

    feat: Add Department Entity

--- src/main/java/com/lec/payroll/model/Department.java
+++ src/main/java/com/lec/payroll/model/Department.java
@@ -1,4 +1,23 @@
 package com.lec.payroll.model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "departments")
 public class Department {
+    @Id
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @OneToMany(mappedBy = "department", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Employee> employees;
 }
