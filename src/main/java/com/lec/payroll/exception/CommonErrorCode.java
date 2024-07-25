79e307c64ca53be93b980e2f09afbdf6964521f5
minisun <minisundev@gmail.com>
2024-07-25 14:58:21 +0900

    feat: Add CommonErrorCode

---
+++ src/main/java/com/lec/payroll/exception/CommonErrorCode.java
@@ -0,0 +1,20 @@
+package com.lec.payroll.exception;
+
+public enum CommonErrorCode implements ErrorCode{
+    ;
+
+    @Override
+    public String getCode() {
+        return "";
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+
+    @Override
+    public String getHttpStatus() {
+        return "";
+    }
+}
