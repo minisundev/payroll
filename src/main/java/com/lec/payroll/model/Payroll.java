ee4b80612bae5b18c5f49748af73ac734ed512ba
minisun <minisundev@gmail.com>
2024-07-25 14:19:03 +0900

    feat: Add Payroll Entity

--- src/main/java/com/lec/payroll/model/Payroll.java
+++ src/main/java/com/lec/payroll/model/Payroll.java
@@ -1,4 +1,24 @@
 package com.lec.payroll.model;
 
-public class Payroll {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "payrolls")
+public class Payroll extends BaseEntity {
+    @Id
+    private Long id;
+
+    private Long paycheck;
+
+    @OneToOne
+    private Employee employee;
+
 }
