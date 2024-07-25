c601e1074f0bbae609ef06ec73c258192a620378
minisun <minisundev@gmail.com>
2024-07-25 17:43:41 +0900

    feat: Add Builder to EmployeeResponse

--- src/main/java/com/lec/payroll/domain/employee/dto/response/EmployeeResponse.java
+++ src/main/java/com/lec/payroll/domain/employee/dto/response/EmployeeResponse.java
@@ -2,7 +2,9 @@ package com.lec.payroll.domain.employee.dto.response;
 
 import com.lec.payroll.domain.employee.model.Department;
 import com.lec.payroll.domain.employee.model.Position;
+import lombok.Builder;
 
+@Builder
 public record EmployeeResponse(
         Long id,
         String name,
