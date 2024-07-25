0ba1e5234d638fc31c259c1889ab75b853957e22
minisun <minisundev@gmail.com>
2024-07-25 17:48:50 +0900

    feat: Add addPayroll to PayrollService

--- src/main/java/com/lec/payroll/domain/payroll/service/PayrollService.java
+++ src/main/java/com/lec/payroll/domain/payroll/service/PayrollService.java
@@ -1,11 +1,39 @@
 package com.lec.payroll.domain.payroll.service;
 
+import com.lec.payroll.domain.employee.dto.response.EmployeeResponse;
+import com.lec.payroll.domain.employee.model.Employee;
+import com.lec.payroll.domain.employee.repository.EmployeeRepository;
+import com.lec.payroll.domain.global.exception.CommonErrorCode;
+import com.lec.payroll.domain.global.exception.GlobalException;
+import com.lec.payroll.domain.payroll.dto.request.PayrollCreateRequest;
+import com.lec.payroll.domain.payroll.dto.response.PayrollResponse;
+import com.lec.payroll.domain.payroll.model.Payroll;
 import com.lec.payroll.domain.payroll.repository.PayrollRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class PayrollService {
     private final PayrollRepository payrollRepository;
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+
+    public void addPayroll(PayrollCreateRequest request) {
+        verifyEmployeeExistence(request.employeeId());
+        Payroll payroll = modelMapper.map(request, Payroll.class);
+        payrollRepository.save(payroll);;
+    }
+
+    private void verifyEmployeeExistence(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()-> new GlobalException(CommonErrorCode.NOT_FOUND));
+    }
+
+    private Payroll findPayrollById(Long payrollId){
+        Payroll payroll = payrollRepository.findById(payrollId)
+                .orElseThrow(()-> new GlobalException(CommonErrorCode.NOT_FOUND));
+        return payroll;
+    }
 }
