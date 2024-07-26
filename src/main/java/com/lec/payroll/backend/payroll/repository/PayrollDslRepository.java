a9a10679b52a7a4921ea5848e60f4728f3caf291
minisun <minisundev@gmail.com>
2024-07-26 14:24:35 +0900

    feat: Add getAllPayrolls method to PayrollDslRepository

--- src/main/java/com/lec/payroll/backend/payroll/repository/PayrollDslRepository.java
+++ src/main/java/com/lec/payroll/backend/payroll/repository/PayrollDslRepository.java
@@ -2,8 +2,13 @@ package com.lec.payroll.backend.payroll.repository;
 
 
 import com.lec.payroll.backend.employee.model.Department;
+import com.lec.payroll.backend.payroll.model.Payroll;
+
+import java.util.List;
 
 public interface PayrollDslRepository {
 
     public Integer getTotalPayrollsByDepartment(Department department);
+
+    public List<Payroll> getAllPayrolls(int page, int size);
 }
--- src/main/java/com/lec/payroll/backend/payroll/repository/PayrollDslRepositoryImpl.java
+++ src/main/java/com/lec/payroll/backend/payroll/repository/PayrollDslRepositoryImpl.java
@@ -1,11 +1,17 @@
 package com.lec.payroll.backend.payroll.repository;
 
 import com.lec.payroll.backend.employee.model.Department;
+import com.lec.payroll.backend.payroll.model.Payroll;
 import com.lec.payroll.backend.payroll.model.QPayroll;
 import com.lec.payroll.backend.employee.model.QEmployee;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -25,5 +31,19 @@ public class PayrollDslRepositoryImpl implements PayrollDslRepository{
                 .join(payroll.employee, employee)
                 .where(employee.department.eq(department))
                 .fetchOne();
+    }
+
+    @Override
+    public List<Payroll> getAllPayrolls(int page, int size) {
+        QPayroll payroll = QPayroll.payroll;
+        int offset = page*size;
+
+        List<Payroll> payrolls = jpaQueryFactory
+                .selectFrom(payroll)
+                .offset(offset)
+                .limit(size)
+                .fetch();
+
+        return payrolls;
     }
 }
